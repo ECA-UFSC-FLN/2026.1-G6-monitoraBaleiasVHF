18ae9b3dcf2ffb4f0ecdc25497def7456e591c45
Maria Eduarda Justus Franco da Luz <maria.justus.luz@gmail.com>
2026-04-27 10:44:10 -0300

    Sinal 1 apenas

--- experimentos/main2.py
+++ experimentos/main2.py
@@ -14,28 +14,10 @@ VELOCIDADE_LUZ = 299792458.0
 import numpy as np
 from scipy.signal import butter, lfilter
 
-# --- Criação do Filtro Passa-Baixa para Dados Complexos ---
-def aplicar_filtro_passa_baixa(sinal, sample_rate, corte_hz):
-    """
-    Aplica um filtro passa-baixa no sinal complexo.
-    Para dados I/Q em banda base, um corte_hz de 200000 (200 kHz)
-    vai manter as frequências de -200 kHz até +200 kHz.
-    """
-    nyq = 0.5 * sample_rate
-    frequencia_corte_normalizada = corte_hz / nyq
-    
-    # Cria um filtro Butterworth passa-baixa de ordem 4
-    b, a = butter(4, frequencia_corte_normalizada, btype='low')
-    
-    # Aplica o filtro no sinal complexo
-    sinal_filtrado = lfilter(b, a, sinal)
-    return sinal_filtrado
-
-
 
 # Criando os dados de teste
 sample_rate = 2.048e6     # MS/s
-freq_sinal = 433.9     # MHz 434.041
+freq_sinal = 434.016     # MHz
 lambda_onda = VELOCIDADE_LUZ / freq_sinal
 d_espacamento = lambda_onda / 2
 print(f"Espaçamento desejado: {d_espacamento}")
@@ -48,48 +30,40 @@ sdr1.gain = 'auto'
 N_SAMPLES = 32 * 1024
 N_FFT = 1024
 
-# --- Como usar no seu código ---
-# Limite de +- 0.2 MHz = 200.000 Hz
-limite_banda_hz = 50000  
 
-# Aplica o filtro no sinal original
 sinal_A1 = sdr1.read_samples(N_SAMPLES)
-#sinal_A1_limpo = aplicar_filtro_passa_baixa(sinal_A1, sample_rate, limite_banda_hz)
 sdr1.close()
 print("Encerrado")
 
-sdr1 = RtlSdr(device_index=0)
-sdr1.sample_rate = sample_rate  # 2.048 MS/s
-sdr1.center_freq = freq_sinal  # 101.3 MHz
-sdr1.gain = 'auto'
-sinal_A2 = sdr1.read_samples(N_SAMPLES)
+# sdr2 = RtlSdr(device_index=0)
+# sdr2.sample_rate = sample_rate  # 2.048 MS/s
+# sdr2.center_freq = freq_sinal  # 101.3 MHz
+# sdr2.gain = 'auto'
 
-sub = sinal_A1 - sinal_A2
+# sinal_A2 = sdr2.read_samples(N_SAMPLES)
+# sdr2.close()
+
 
 plt.figure(figsize=(12, 8))
 
 # Mostra a senoide
-onda1 = np.real(sinal_A1[:100])
+onda1 = np.abs(sinal_A1[:250])
 
 plt.subplot(2, 1, 1)
-plt.plot(sub, color='green', label='Subtração')
+plt.plot(onda1, color='red', label='Sinal A1')
 plt.title("Sinal no Domínio do Tempo")
 plt.xlabel("Amostras")
 plt.ylabel("Amplitude")
 plt.legend()
 plt.grid(True)
 
-plt.subplot(2, 1, 1)
-plt.plot(sinal_A1, color='red', label='Com sinal')
-plt.legend()
-
-plt.subplot(2, 1, 1)
-plt.plot(sinal_A2, color='blue', label='Sem sinal')
-plt.legend()
+# plt.subplot(2, 1, 1)
+# plt.plot(sinal_A2, color='blue', label='Sinal A2')
+# plt.legend()
 
 # 2. Sinal no Domínio da Frequência (Espectro de Potência)
 plt.subplot(2, 1, 2)
-plt.psd(sub, NFFT=N_FFT, Fs=sample_rate/1e6, Fc=freq_sinal/1e6)  # Convertendo para MHz
+plt.psd(sinal_A1, NFFT=N_FFT, Fs=sample_rate/1e6, Fc=freq_sinal/1e6)  # Convertendo para MHz
 plt.title("Densidade Espectral de Potência (PSD)")
 plt.xlabel("Frequência (MHz)")
 plt.ylabel("Intensidade (dB/Hz)")
